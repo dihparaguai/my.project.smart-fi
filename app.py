e557d6329186240aa44ba2e24f0dccf509916c4c
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-12-11 12:54:58 -0300

    arquivo principal de entrada

---
+++ app.py
@@ -0,0 +1,49 @@
+from modules.user import User
+from modules.category import Category
+from modules.payment_type import Payment_type
+
+from modules.transaction import Transaction  
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+user = User(1, "Diego", "diego@example.com", "senha123")
+category = Category(1, "Alimentação", "Gastos com comida")
+payment_type = Payment_type(1, "Credito", "Diego Paraguai")
+
+transaction = Transaction(
+    user=                       user,
+    category=                   category,
+    payment_type=               payment_type,
+    
+    transaction_id=             1,
+    fact_date=                  "2024-12-09",
+    payment_date=               "2024-12-10",
+    description=                "Compra de supermercado",
+    transaction_type=           "expense",
+    value=                      150.75
+)
+
+print(transaction)
+
+
+
+
+@app.route("/transaction-list")
+def transaction_list():
+    
+    return render_template(
+        "transaction_list.html",
+        payment_date=       transaction.payment_date, 
+        description=        transaction.description, 
+        value=              transaction.value, 
+        payment_type=       transaction.payment_type, 
+        transaction_type=   transaction.transaction_type,
+        fact_date=          transaction.fact_date
+    )  
+
+
+
+app.run(debug=True)
