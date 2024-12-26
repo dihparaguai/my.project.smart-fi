e9f7c3fc87725d94ddfbd61bf6c11c523c29d5f2
Diego Paraguai <diego.paraguai.dp@gmail.com>
2024-12-26 13:12:36 -0300

    Desenvolvidas páginas 'user_login.html' e 'user_register.html'. Implementadas rota 'user_register()' e rota intermediária '_user_register()'.

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from modules.category import Category
 from modules.payment_type import PaymentType
 from modules.transaction import Transaction
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, flash
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '@chave_secreta@'
 
 
-user = User(1, "Diego", "diego@example.com", "senha123")
-
+user1 = User(1, "Diego", "diego@example.com", "senha123")
+user2 = User(2, "Rodrigo", "rodrigo@example.com", "senha456")
+user_dict = {
+    user1.id: user1,
+    user2.id: user2
+}
 
 category1 = Category(1, "Alimentação", "Gastos com comida")
 category2 = Category(2, "Lazer", "Gastos com parque")
@@ -26,8 +31,8 @@ payment_type_dict = {
 }
 
 
-transaction1 = Transaction(user, category1, payment_type1, 1, "2024-12-09", "2024-12-10", "Compra de supermercado", "expense", 150.75)
-transaction2 = Transaction(user, category2, payment_type2, 2, "2023-11-08", "2022-09-11", "Compra de ...", "income", 0.75)
+transaction1 = Transaction(user2, category1, payment_type1, 1, "2024-12-09", "2024-12-10", "Compra de supermercado", "expense", 150.75)
+transaction2 = Transaction(user1, category2, payment_type2, 2, "2023-11-08", "2022-09-11", "Compra de ...", "income", 0.75)
 transaction_dict = {
     transaction1.id: transaction1,
     transaction2.id: transaction2
@@ -57,7 +62,7 @@ def _transaction_register():
     payment_type_id = int(request.form.get('payment_type'))
 
     transaction = Transaction(
-        user=user,
+        # user=
         category=category_dict[category_id],
         payment_type=payment_type_dict[payment_type_id],
 
@@ -98,6 +103,40 @@ def _transaction_update():
 def _transaction_delete(id):
     transaction_dict.pop(id)
     return redirect(url_for("transaction_history"))
+
+
+@app.route("/user-login")
+def user_login():
+    return render_template("user_login.html")
+
+
+@app.route("/_user-login")
+def _user_login():
+    return redirect(url_for("/"))
+
+
+@app.route("/user-register")
+def user_register():
+    return render_template("user_register.html")
+
+
+@app.route("/_user-register", methods=['POST'])
+def _user_register():
+    
+    email = request.form['email']
+    name = request.form['name']
+    password = request.form['password']
+    password_retyped = request.form['password_retyped']
+    
+    if password != password_retyped:
+        flash('Senhas não foram digitadas iguais', 'error')
+        return redirect(url_for("user_register"))
+        
+    user = User(id=3, email=email, name=name, password=password)
+    
+    user_dict[user.id] = user
+    flash('Usuario cadatradado com sucesso', 'success') 
+    return redirect(url_for("user_login"))
 
 
 app.run(debug=True)
